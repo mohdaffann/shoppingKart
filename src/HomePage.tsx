ca6428c1488278e63c2fe4cee05728cc86125c92
muhammadaffan <muhammadaffanpvt@gmail.com>
2026-07-25 23:32:24 +0530

    Homepage

---
+++ src/HomePage.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+function HomePage() {
+    return (
+        <div className="max-w-full flex items-center justify-center">
+            <h1 className="font-bold  text-4xl">Home Pagee</h1>
+        </div>
+    )
+}
+
+export default HomePage;
